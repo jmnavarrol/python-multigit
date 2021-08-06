f6eab280a4047dbc0412b5aec936da29073d0b99
Jesús M. Navarro <jesus.navarro@undominio.net>
2021-08-06 16:01:17 +0200

    Clone/Update processes refactored to include updates when needed.

--- src/multigit/__init__.py
+++ src/multigit/__init__.py
@@ -108,94 +108,68 @@ def manage_subrepo(subrepo):
 	try:
 		repo = Repo(subrepo['path'])
 	except git_exception.NoSuchPathError as e:
-		# repo not yet downloaded
+		# repo not yet cloned
+		branch_like = None
+		if 'branch' in subrepo:
+			branch_like = branch=subrepo['branch']
+		elif 'tag' in subrepo:
+			branch_like = branch=subrepo['tag']
+			
 		try:
-			if 'branch' in subrepo:
-				try:
-					repo = Repo.clone_from(subrepo['repo'], subrepo['path'], branch=subrepo['branch'])
-				except git_exception.GitCommandError as e:
-					print(Style.BRIGHT + Fore.RED + "ERROR:", end=' ')
-					print(Style.BRIGHT + e.stderr)
-					sys.exit(errno.EBADE)
-			elif 'tag' in subrepo:
-				try:
-					repo = Repo.clone_from(subrepo['repo'], subrepo['path'], branch=subrepo['tag'])
-				except git_exception.GitCommandError as e:
-					print(Style.BRIGHT + Fore.RED + "ERROR:", end=' ')
-					print(Style.BRIGHT + e.stderr)
-					sys.exit(errno.EBADE)
-			elif 'commit' in subrepo:
-				try:
-					repo = Repo.clone_from(subrepo['repo'], subrepo['path'])
-				except git_exception.GitCommandError as e:
-					print(Style.BRIGHT + Fore.RED + "ERROR:", end=' ')
-					print(Style.BRIGHT + e.stderr)
-					sys.exit(errno.EBADE)
+			if branch_like:
+				repo = Repo.clone_from(subrepo['repo'], subrepo['path'], branch=branch_like)
 			else:
-				try:
-					repo = Repo.clone_from(subrepo['repo'], subrepo['path'])
-				except git_exception.GitCommandError as e:
-					print(Style.BRIGHT + Fore.RED + "ERROR:", end=' ')
-					print(Style.BRIGHT + e.stderr)
-					sys.exit(errno.EBADE)
+				repo = Repo.clone_from(subrepo['repo'], subrepo['path'])
 		except git_exception.GitCommandError as e:
-			print(e.stderr)
-			sys.exit(errno.ENOENT)
+			print(Style.BRIGHT + Fore.RED + "ERROR:", end=' ')
+			print(Style.BRIGHT + e.stderr)
+			sys.exit(errno.EBADE)
 			
 		print(Style.BRIGHT + Fore.GREEN + "INFO:", end=' ')
-		print("Repo " + Style.BRIGHT + "'" + subrepo['repo'] + "'", end=' ')
-		print("cloned at " + Style.BRIGHT + "'" + subrepo['path'] + "'", end='.\n')
+		print("Repo " + Style.BRIGHT + "'" + subrepo['repo'] + "'")
+		print("\tCloned at " + Style.BRIGHT + "'" + subrepo['path'] + "'")
 		
-	# checkout gitref if requested and possible
+	# Update sandbox if needed and possible
+	current_commit = repo.commit().hexsha
+	repo.remotes.origin.fetch()
+	
 	if any(gitref in subrepo for gitref in SUBREPO_FORMAT['GITREF']):
+		if 'branch' in subrepo:
+			desired_commit = str(repo.commit('origin/' + subrepo['branch']))
+			gitref = subrepo['branch']
+		elif 'tag' in subrepo:
+			desired_commit = str(repo.commit(subrepo['tag']))
+			gitref = subrepo['tag']
+		elif 'commit' in subrepo:
+			desired_commit = str(subrepo['commit'])
+			gitref = subrepo['commit']
+		else:
+			error_line = str(sys._getframe().f_lineno - 1)
+			print(Style.BRIGHT + Fore.RED + "INTERNAL ERROR:", end=' ')
+			print("wrong gitref type at " + Style.BRIGHT + "'" + os.path.abspath(__file__) + Style.BRIGHT + "'", end=', ')
+			print("line number " + Style.BRIGHT + "'" + error_line + "'", end='.\n')
+			sys.exit(errno.EINVAL)
+	else:
+		desired_commit = repo.remotes.origin.refs.HEAD.commit.hexsha
+		gitref = repo.git.symbolic_ref('refs/remotes/origin/HEAD').replace('refs/remotes/origin/','')
+		
+	# The sandbox update itself
+	if current_commit != desired_commit:
 		if not repo.is_dirty():
-			current_commit = str(repo.head.commit)
-			repo.remotes.origin.fetch()
-			if 'branch' in subrepo:
-				desired_commit = str(repo.commit('origin/' + subrepo['branch']))
-				if current_commit != desired_commit:
-					try:
-						repo.git.checkout(subrepo['branch'])
-					except git_exception.GitCommandError as e:
-						print(Style.BRIGHT + Fore.RED + "ERROR:", end=' ')
-						print(Style.BRIGHT + e.stderr)
-						sys.exit(errno.EBADE)
-					print(Style.BRIGHT + Fore.GREEN + "INFO:", end=' ')
-					print("Repo at " + Style.BRIGHT + "'" + subrepo['path'] + "'", end='.\n')
-					print("\tupdated: " + Style.BRIGHT + "'" + current_commit + "'", end=' -> ')
-					print(Style.BRIGHT + "'" + desired_commit + "'")
-			elif 'tag' in subrepo:
-				desired_commit = str(repo.commit(subrepo['tag']))
-				if current_commit != desired_commit:
-					try:
-						repo.git.checkout(subrepo['tag'])
-					except git_exception.GitCommandError as e:
-						print(Style.BRIGHT + Fore.RED + "ERROR:", end=' ')
-						print(Style.BRIGHT + e.stderr)
-						sys.exit(errno.EBADE)
-					print(Style.BRIGHT + Fore.GREEN + "INFO:", end=' ')
-					print("Repo at " + Style.BRIGHT + "'" + subrepo['path'] + "'", end='.\n')
-					print("\tupdated: " + Style.BRIGHT + "'" + current_commit + "'", end=' -> ')
-					print(Style.BRIGHT + "'" + desired_commit + "'")
-			elif 'commit' in subrepo:
-				desired_commit = str(subrepo['commit'])
-				if current_commit != desired_commit:
-					try:
-						repo.git.checkout(subrepo['commit'])
-					except git_exception.GitCommandError as e:
-						print(Style.BRIGHT + Fore.RED + "ERROR:", end=' ')
-						print(Style.BRIGHT + e.stderr)
-						sys.exit(errno.EBADE)
-					print(Style.BRIGHT + Fore.GREEN + "INFO:", end=' ')
-					print("Repo at " + Style.BRIGHT + "'" + subrepo['path'] + "'", end='.\n')
-					print("\tupdated: " + Style.BRIGHT + "'" + current_commit + "'", end=' -> ')
-					print(Style.BRIGHT + "'" + desired_commit + "'")
-			else:
-				error_line = str(sys._getframe().f_lineno - 1)
-				print(Style.BRIGHT + Fore.RED + "INTERNAL ERROR:", end=' ')
-				print("wrong gitref type at " + Style.BRIGHT + "'" + os.path.abspath(__file__) + Style.BRIGHT + "'", end=', ')
-				print("line number " + Style.BRIGHT + "'" + error_line + "'", end='.\n')
-				sys.exit(errno.EINVAL)
+			try:
+				repo.git.checkout(gitref)
+			except git_exception.GitCommandError as e:
+				print(Style.BRIGHT + Fore.RED + "ERROR:", end=' ')
+				print(Style.BRIGHT + e.stderr)
+				sys.exit(errno.EBADE)
+				
+			if not repo.head.is_detached:
+				repo.git.pull()
+				
+			print(Style.BRIGHT + Fore.GREEN + "INFO:", end=' ')
+			print("Repo at " + Style.BRIGHT + "'" + subrepo['path'] + "'")
+			print("\tUpdated: " + Style.BRIGHT + "'" + current_commit + "'", end=' -> ')
+			print(Style.BRIGHT + "'" + desired_commit + "'")
 		else:
 			print(Style.BRIGHT + Fore.YELLOW + "WARNING:", end=' ')
 			print("at " + Style.BRIGHT + "'" + subrepo['path'] + "'")
