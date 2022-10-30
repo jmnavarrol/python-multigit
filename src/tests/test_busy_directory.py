1d11b881f9163d1af518a3d5db1bd09f7b220310
Jesús M. Navarro <jesusmnavarrolopez@gmail.com>
2022-10-30 16:04:23 +0100

    unit tests: both cmd-related tests unified.

--- src/tests/test_busy_directory.py
+++ src/tests/test_busy_directory.py
@@ -6,32 +6,47 @@ import unittest
 import os, shutil
 
 class BusyDirectory(unittest.TestCase):
+	
 	# sets up the environment needed for the test
+	@classmethod
 	def setUp(self):
-	# Creates a test directory
+		# Creates a test directory
 		self.tests_dir = os.path.dirname(os.path.realpath(__file__))
 		self.tmp_path = os.path.dirname(os.path.realpath(__file__)) + '/test'
 		if not os.path.exists(self.tmp_path):
 			os.makedirs(self.tmp_path)
 			
-	# Adds a custom subrepos file and contents to the test path
+		# Adds a custom subrepos file and contents to the test path
 		shutil.copy(
 			self.tests_dir + '/subrepos.empty_repo',
 			os.path.join(self.tmp_path, 'subrepos')
 		)
-		if not os.path.exists(self.tmp_path + '/empty-repo'):
-			os.makedirs(self.tmp_path + '/empty-repo')
-			
+		
+		
+	def test_not_yet_cloned(self):
+		# Should show a proper 'not cloned' message for this repository
+		os.chdir(self.tmp_path)
+		result = os.system('multigit --status')
+		self.assertEqual(result, 0)
+		
 		
 	def test_busy_directory(self):
-	# Should throw an ERROR message since git won't be able to clone to an already in use dir
+		# Creates a "busy directory"
+		if not os.path.exists(self.tmp_path + '/empty-repo'):
+			os.makedirs(self.tmp_path + '/empty-repo')
+		# ...with a file within
+		with open(self.tmp_path + '/empty-repo/readme.txt', 'w') as f:
+			f.write('Simple text file')
+			
+		# Should throw an ERROR message since git won't be able to clone to an already in use dir
 		os.chdir(self.tmp_path)
 		result = os.system('multigit --run')
 		self.assertEqual(result, 0)
 		
 		
+	@classmethod
 	def tearDown(self):
-	# clean up after the test
+		# clean up after the test
 		shutil.rmtree(self.tmp_path)
 		
 		
