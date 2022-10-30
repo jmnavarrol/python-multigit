bccdb19e33f30f9e56b72843c3db22aac4b674af
Jesús M. Navarro <jesusmnavarrolopez@gmail.com>
2022-10-30 14:12:35 +0100

    busy_directory test refactored to 'make space' for a new test.

--- src/tests/test_busy_directory.py
+++ src/tests/test_busy_directory.py
@@ -1,9 +1,11 @@
+# tests trying to clone a repository into an already busy directory
+
 import unittest
 
 # Other dependencies
 import os, shutil
 
-class MultigitTest(unittest.TestCase):
+class BusyDirectory(unittest.TestCase):
 	# sets up the environment needed for the test
 	def setUp(self):
 	# Creates a test directory
@@ -14,15 +16,15 @@ class MultigitTest(unittest.TestCase):
 			
 	# Adds a custom subrepos file and contents to the test path
 		shutil.copy(
-			self.tests_dir + '/subrepos.busy_directory',
+			self.tests_dir + '/subrepos.empty_repo',
 			os.path.join(self.tmp_path, 'subrepos')
 		)
-		if not os.path.exists(self.tmp_path + '/busydir'):
-			os.makedirs(self.tmp_path + '/busydir')
+		if not os.path.exists(self.tmp_path + '/empty-repo'):
+			os.makedirs(self.tmp_path + '/empty-repo')
 			
 		
 	def test_busy_directory(self):
-	# Should throw an ERROR since git won't be able to clone to an already in use dir
+	# Should throw an ERROR message since git won't be able to clone to an already in use dir
 		os.chdir(self.tmp_path)
 		result = os.system('multigit --run')
 		self.assertEqual(result, 0)
