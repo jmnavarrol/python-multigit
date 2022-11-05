0193c61807134e6a11f4c49e2b9de1e04158a957
Jesús M. Navarro <jesusmnavarrolopez@gmail.com>
2022-11-05 11:14:03 +0100

    Closing version v0.11.1.

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,6 @@
 # Workaround allow editable mode pip install along pyproject.toml file
 # See https://stackoverflow.com/questions/62983756/what-is-pyproject-toml-file-for
 from setuptools import setup
-#import setuptools
 
 if __name__ == "__main__":
 	setup()
